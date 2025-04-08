de9ef931cd1359412dbb81c72eeccca6f1db0d8a
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-04-08 15:20:22 +0200

    fix: Change openapi cfg

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/config/config/OpenApiCFG.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/config/config/OpenApiCFG.java
@@ -18,7 +18,11 @@ import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
@@ -27,66 +31,54 @@ import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
+@OpenAPIDefinition(
+	info = @Info(
+			extensions = {
+				@Extension(properties = {
+					@ExtensionProperty(name = "x-api-id", value = "1"),
+					@ExtensionProperty(name = "x-summary", value = "Gateway Config")
+				})
+			},
+			title = "Gateway Config", 
+			version = "1.0.0", 
+			description = "Gateway Config",
+			termsOfService = "${docs.info.termsOfService}", 
+			contact = @Contact(name = "${docs.info.contact.name}", url = "${docs.info.contact.url}", email = "${docs.info.contact.mail}")))
 public class OpenApiCFG {
 
-	public OpenApiCFG() {
-		// Empty constructor.
+
+  public OpenApiCFG() {
+  }
+
+  @Bean
+	public OpenApiCustomizer openApiCustomiser() {
+		return openApi -> openApi.getComponents().getSchemas().values().forEach( s -> s.setAdditionalProperties(false));
 	}
 	
 	@Bean
-	public OpenApiCustomizer openApiCustomiser(final CustomSwaggerCFG customOpenapi) {
-
-
-		return openApi -> {
-
-			// Populating info section.
-			openApi.getInfo().setTitle(customOpenapi.getTitle());
-			openApi.getInfo().setVersion(customOpenapi.getVersion());
-			openApi.getInfo().setDescription(customOpenapi.getDescription());
-			openApi.getInfo().setTermsOfService(customOpenapi.getTermsOfService());
-
-			// Adding contact to info section
-			final Contact contact = new Contact();
-			contact.setName(customOpenapi.getContactName());
-			contact.setUrl(customOpenapi.getContactUrl());
-			contact.setEmail(customOpenapi.getContactMail());
-			openApi.getInfo().setContact(contact);
-
-			// Adding extensions
-			openApi.getInfo().addExtension("x-api-id", customOpenapi.getApiId());
-			openApi.getInfo().addExtension("x-summary", customOpenapi.getApiSummary());
-
-			for (final Server server : openApi.getServers()) {
-				final Pattern pattern = Pattern.compile("^https://.*");
-				if (!pattern.matcher(server.getUrl()).matches()) {
-					server.addExtension("x-sandbox", true);
-				}
-			}
-
-			openApi.getComponents().getSchemas().values().forEach(schema -> {
-				schema.setAdditionalProperties(false);
-			});
-
-		};
-	}
-
-	@Bean
 	public OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
 		return openApi -> {
+			for (final Server server : openApi.getServers()) {
+                final Pattern pattern = Pattern.compile("^https://.*");
+                if (!pattern.matcher(server.getUrl()).matches()) {
+                    server.addExtension("x-sandbox", true);
+                }
+            }
+			
 			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
-				final ApiResponses apiResponses = operation.getResponses();
-
-				final Schema<Object> errorResponseSchema = new Schema<>();
+				ApiResponses apiResponses = operation.getResponses();
+				
+				Schema<Object> errorResponseSchema = new Schema<>();
 				errorResponseSchema.setName("Error");
 				errorResponseSchema.set$ref("#/components/schemas/ErrorResponseDTO");
-				final MediaType media = new MediaType();
+				MediaType media =new MediaType();
 				media.schema(errorResponseSchema);
-				final ApiResponse apiResponse = new ApiResponse().description("default")
-						.content(new Content()
-								.addMediaType(org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-										media));
+				ApiResponse apiResponse = new ApiResponse().description("default")
+				        .content(new Content()
+	                                .addMediaType(org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE, media));
 				apiResponses.addApiResponse("default", apiResponse);
 			}));
 		};
 	}
 }
+
