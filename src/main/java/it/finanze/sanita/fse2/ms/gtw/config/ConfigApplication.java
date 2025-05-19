2cfe643d77e2f09a0318926a54df6a663b287a31
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-05-19 14:50:37 +0200

    fix(Tests): Define bean restTemplate

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/config/ConfigApplication.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/config/ConfigApplication.java
@@ -11,8 +11,12 @@
  */
 package it.finanze.sanita.fse2.ms.gtw.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConfigApplication {
@@ -21,4 +25,14 @@ public class ConfigApplication {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
 
+	 /**
+     * Definizione rest template.
+     * 
+     * @return	rest template
+     */
+    @Bean 
+    @Qualifier("restTemplate")
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
